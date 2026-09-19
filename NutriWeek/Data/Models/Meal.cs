2c2c86a1d22a88233c7b9fadbdee7fbcf731c929
Aleksandra <slsandra2411@gmail.com>
2026-09-20 00:56:16 +0200

    Add Meal Entity

---
+++ NutriWeek/Data/Models/Meal.cs
@@ -0,0 +1,18 @@
+﻿using NutriWeek.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace NutriWeek.Data.Models
+{
+    public class Meal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public MealType MealType { get; set; }
+
+        public int DishId { get; set; }
+
+        public Dish Dish { get; set; } = null!;
+
+    }
+}
