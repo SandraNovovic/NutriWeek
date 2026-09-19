a6025f974cd7a029a48978337dee77e54a98b8cb
Aleksandra <slsandra2411@gmail.com>
2026-09-20 01:27:39 +0200

    Add DailyMenu entity

---
+++ NutriWeek/Data/Models/DailyMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NutriWeek.Data.Models
+{
+    public class DailyMenu
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        public DateTime Date { get; set; }
+
+        public int WeeklyMenuId { get; set; }
+
+        public WeeklyMenu WeeklyMenu { get; set; } = null!;
+
+        public ICollection<Meal> Meals { get; set; } = new HashSet<Meal>();
+    }
+}
