8c32abdcda3f085b45cef248f8f9b6c01d0b1d64
Aleksandra <slsandra2411@gmail.com>
2026-09-20 13:37:07 +0200

    Register database.

--- NutriWeek/Program.cs
+++ NutriWeek/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NutriWeek.Data;
+
 namespace NutriWeek
 {
     public class Program
@@ -5,6 +8,9 @@ namespace NutriWeek
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<NutriWeekDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
