047f2d5aa18cd590d4f9a025af58841afc599d52
Aleksandra <slsandra2411@gmail.com>
2026-09-20 13:28:55 +0200

    Add database context.

---
+++ NutriWeek/Data/NutriWeekDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NutriWeek.Data.Models;
+
+namespace NutriWeek.Data
+{
+    public class NutriWeekDbContext : DbContext
+    {
+
+        public NutriWeekDbContext(DbContextOptions<NutriWeekDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Dish> Dishes { get; set; } = null!;    
+
+        public DbSet<WeeklyMenu> WeeklyMenus { get; set; } = null!;
+
+        public DbSet<DailyMenu> DailyMenus { get; set; } = null!;
+
+        public DbSet<Meal> Meals { get; set; } = null!;
+    }
+}
