83ab506525c87108e1eb2dc1cb40384ec2679493
Aleksandra <slsandra2411@gmail.com>
2026-09-20 14:07:42 +0200

    Create controller classes for the entities.

---
+++ NutriWeek/Controllers/DishesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NutriWeek.Controllers
+{
+    public class DishesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NutriWeek/Controllers/WeeklyMenusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NutriWeek.Controllers
+{
+    public class WeeklyMenusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
