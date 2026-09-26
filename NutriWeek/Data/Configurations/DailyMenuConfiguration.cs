5259169be5baa2ae5ea3c34d874cbfe2b2b0b5f6
Aleksandra <slsandra2411@gmail.com>
2026-09-26 12:20:28 +0200

    Add DailyMenu configuration and seed data

---
+++ NutriWeek/Data/Configurations/DailyMenuConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NutriWeek.Data.Models;
+
+namespace NutriWeek.Data.Configurations
+{
+    public class DailyMenuConfiguration : IEntityTypeConfiguration<DailyMenu>
+    {
+        public readonly IEnumerable<DailyMenu> seedDailyMenus = new List<DailyMenu>
+        {
+                new DailyMenu
+                {
+                    Id = 1,
+                    Date = new DateTime(2026, 9, 21),
+                    WeeklyMenuId = 1
+                },
+                new DailyMenu
+                {
+                    Id = 2,
+                    Date = new DateTime(2026, 9, 22),
+                    WeeklyMenuId = 1
+                }
+        };
+
+        public void Configure(EntityTypeBuilder<DailyMenu> builder)
+        {
+            builder.HasData(seedDailyMenus);
+        }
+    }
+}
