b791cb99838281d88d490b5a3aa03da49460a6d6
Aleksandra <slsandra2411@gmail.com>
2026-09-26 12:19:17 +0200

    Add WeeklyMenu configuration and seed data

---
+++ NutriWeek/Data/Configurations/WeeklyMenuConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NutriWeek.Data.Models;
+
+namespace NutriWeek.Data.Configurations
+{
+    public class WeeklyMenuConfiguration
+    {
+        public readonly IEnumerable<WeeklyMenu> seedWeeklyMenus = new List<WeeklyMenu>
+        {
+
+          new WeeklyMenu
+          {
+              Id = 1,
+              Title = "Healthy Week",
+              WeekStartDate = new DateTime(2026, 9, 21)
+          }
+
+        };
+        public void Configure(EntityTypeBuilder<WeeklyMenu> builder)
+        {
+            builder.HasData(seedWeeklyMenus);
+        }
+    }
+}
